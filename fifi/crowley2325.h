128f259f46b66b89ed61ec993787ec3a24596565
Morten V. Pedersen <morten@mortenvp.dk>
2012-05-29 03:57:51 +0200

    Working on the prime2325 implementation

--- fifi/crowley2325.h
+++ fifi/crowley2325.h
@@ -17,59 +17,50 @@ namespace fifi
     /// to the 2^32 - 5 prime field.
     struct crowley2325
     {
-        //crowley2325(
-
-        /// A chunk of data which belongs to a block
-        /// A block may consist of serveral chunks, but
-        /// the total size should stay below the max_block_size().
-        typedef std::pair<const uint8_t*, uint32_t> chunk;
+        /// @returns the maximum size in bytes a block can be
+        static uint32_t max_block_size();
 
         /// @returns the maximum size in bytes a block can be
-        uint32_t max_block_size();
+        static uint32_t max_block_length();
 
-        /// Adds a data chunk to the block
-        /// @param c
-        void add_chunk(const chunk &chunk);
-
-        void add_chunk(const uint8_t* data, uint32_t size);
-
-        /// Searches for a unused bit prefix which can be used to
-        /// pefrom the sub-field mapping
-        /// @return the found prefix
-        uint32_t find_prefix();
-
-        /// Vector to keep track of the different chunks of a
-        /// specific block.
-        std::vector<chunk> m_chunks;
-
-        /// Keeps track of the current block size
-        uint64_t m_block_size;
+        /// @return the prefix length in bits needed for a specific block
+        ///         length
+        static uint32_t prefix_length(uint32_t block_length);
     };
 
     inline uint32_t crowley2325::max_block_size()
     {
-        // 2^29 - 1 data words (where every word is
-        // 32 bits).
-        return 2147483644U;
+        // (2^29 - 2) * 4 to get bytes
+        return 2147483640U;
     }
 
-    inline void crowley2325::add_chunk(const chunk &c)
+    inline uint32_t crowley2325::max_block_length()
     {
-        assert(c.second + m_block_size <= max_block_size());
-
-        m_chunks.push_back(c);
-        m_block_size += c.second;
+        // 2^29 - 2 data words (where every word is 32 bits).
+        return 536870910U;
     }
 
-    inline uint32_t crowley2325::find_prefix()
+    inline uint32_t crowley2325::prefix_length(uint32_t block_length)
     {
-        assert(m_chunks.size() > 0);
-        assert(m_block_size > 0);
+        assert(block_length > 0);
+        assert(block_length <= crowley2325::max_block_length());
 
-        std::vector<uint8_t> m_prefix;
+        // We need enough bits to ensure that we can find a value not
+        // present in the block thus the + 1
+        block_length += 1;
 
+        // In the absence of a log2 function we simply
+        // look for the highest top bit.
+        uint32_t log2;
+        for(log2 = 31; log2 --> 0;)
+        {
+            if(block_length >> log2)
+                break;
+        }
 
+        return log2 + 1;
     }
+
 
 }
 
