4c0c7147ffd766b364af809690f5f1a53fe51f61
Jeppe Pihl <jpihl08@gmail.com>
2014-01-24 00:31:23 +0100

    length_into -> region_info

---
+++ src/fifi/region_info.hpp
@@ -0,0 +1,58 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include "fifi_utils.hpp"
+
+#include <cstdint>
+
+namespace fifi
+{
+
+    /// Produces a field for storing the buffer length
+    template<class Field, class Super>
+    class region_info : public Super
+    {
+    public:
+
+        /// Sets the length in elements
+        /// @param length denotes the new length
+        void set_length(uint32_t length)
+        {
+            m_length = length;
+            m_size = length_to_size<Field>(length);
+        }
+
+        /// Sets the size in bytes
+        /// @param size denotes the new size
+        void set_size(uint32_t size)
+        {
+            m_size = size;
+            m_length = size_to_length<Field>(size);
+        }
+
+        /// Gets the length i.e. the number of elements
+        uint32_t length()
+        {
+            return m_length;
+        }
+
+        /// Gets the size in of the elements in bytes
+        uint32_t size()
+        {
+            return m_size;
+        }
+
+    private:
+
+        /// The length in Field elements
+        uint32_t m_length;
+
+        /// The size in bytes
+        uint32_t m_size;
+
+    };
+}
---
+++ test/src/test_region_info.cpp
@@ -0,0 +1,60 @@
+// Copyright Steinwurf ApS 2011-2012.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#include <gtest/gtest.h>
+
+#include <fifi/binary.hpp>
+#include <fifi/binary4.hpp>
+#include <fifi/binary8.hpp>
+#include <fifi/binary16.hpp>
+#include <fifi/fifi_utils.hpp>
+#include <fifi/prime2325.hpp>
+#include <fifi/region_info.hpp>
+
+namespace fifi
+{
+
+    // Put dummy layers and tests classes in an anonymous namespace
+    // to avoid violations of ODF (one-definition-rule) in other
+    // translation units
+    namespace
+    {
+
+        template<class Super>
+        struct dummy_super : Super
+        { };
+
+        struct dummy_final
+        { };
+    }
+}
+
+template<class Field>
+inline void test_region_info()
+{
+    fifi::dummy_super<fifi::region_info<Field, fifi::dummy_final>> stack;
+
+    for(uint32_t i = 33; i < 63; ++i)
+    {
+        stack.set_length(i);
+        EXPECT_EQ(i, stack.length());
+        EXPECT_EQ(fifi::length_to_size<Field>(i), stack.size());
+
+        stack.set_size(i);
+        EXPECT_EQ(i, stack.size());
+        EXPECT_EQ(fifi::size_to_length<Field>(i), stack.length());
+    }
+}
+
+TEST(TestRegionInfo, api)
+{
+    test_region_info<fifi::binary>();
+    test_region_info<fifi::binary4>();
+    test_region_info<fifi::binary8>();
+
+    // This fails, but why?
+    //test_region_info<fifi::binary16>();
+    //test_region_info<fifi::prime2325>();
+}
