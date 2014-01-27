e69f1013476ca012f5e885740a97c06eabc97bfa
Jeppe Pihl <jpihl08@gmail.com>
2014-01-27 11:43:08 +0100

    tested final

--- test/src/test_final.cpp
+++ test/src/test_final.cpp
@@ -3,10 +3,28 @@
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
 
+#include <fifi/binary.hpp>
+#include <fifi/binary4.hpp>
+#include <fifi/binary8.hpp>
+#include <fifi/binary16.hpp>
+#include <fifi/prime2325.hpp>
+
 #include <fifi/final.hpp>
+
 #include <gtest/gtest.h>
 
-TEST(final, final)
+// Simple test for simple class.
+TEST(test_final, final)
 {
-    std::cout << "final" << std::endl;
+    fifi::final<fifi::binary> f_binary;
+    fifi::final<fifi::binary4> f_binary4;
+    fifi::final<fifi::binary8> f_binary8;
+    fifi::final<fifi::binary16> f_binary16;
+    fifi::final<fifi::prime2325> f_prime2325;
+
+    (void)f_binary;
+    (void)f_binary4;
+    (void)f_binary8;
+    (void)f_binary16;
+    (void)f_prime2325;
 }
