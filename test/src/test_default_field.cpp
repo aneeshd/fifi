2f11f79ec3f589a06ab133bcccdd44e69c5259b9
Jeppe Pihl <jpihl08@gmail.com>
2014-01-29 02:34:20 +0100

    tested default field

--- test/src/test_default_field.cpp
+++ test/src/test_default_field.cpp
@@ -3,10 +3,46 @@
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
 
+#include <type_traits>
 #include <fifi/default_field.hpp>
 #include <gtest/gtest.h>
 
-TEST(TestDefaultField, default_field)
+#include <fifi/simple_online.hpp>
+#include <fifi/full_table.hpp>
+#include <fifi/extended_log_table.hpp>
+#include <fifi/optimal_prime_arithmetic.hpp>
+
+TEST(TestDefaultField, binary_default_field)
 {
-    std::cout << "default_field" << std::endl;
+    bool test= std::is_same<
+        fifi::default_field<fifi::binary>::type,
+        fifi::simple_online<fifi::binary> >::value;
+
+    EXPECT_TRUE(test);
 }
+TEST(TestDefaultField, binary8_default_field)
+{
+    bool test= std::is_same<
+        fifi::default_field<fifi::binary8>::type,
+        fifi::full_table<fifi::binary8> >::value;
+
+    EXPECT_TRUE(test);
+}
+TEST(TestDefaultField, binary16_default_field)
+{
+    bool test= std::is_same<
+        fifi::default_field<fifi::binary16>::type,
+        fifi::extended_log_table<fifi::binary16> >::value;
+
+    EXPECT_TRUE(test);
+}
+TEST(TestDefaultField, prime2325_default_field)
+{
+    bool test= std::is_same<
+        fifi::default_field<fifi::prime2325>::type,
+        fifi::optimal_prime_arithmetic<fifi::prime2325> >::value;
+
+    EXPECT_TRUE(test);
+}
+
+
