39ec71500f48de20651d8a96363a4c89c23e54b5
Jeppe Pihl <jpihl08@gmail.com>
2014-02-18 17:53:16 +0100

    nullptr -> 0

--- src/fifi/label.hpp
+++ src/fifi/label.hpp
@@ -28,17 +28,18 @@ namespace fifi
 
         template<uint32_t SuperLabel>
         auto _get_label(select<false>) const
-            -> decltype(((Super*)nullptr)->template get_label<SuperLabel>())
+            -> decltype(((Super*)0)->template get_label<SuperLabel>())
         {
             return ((Super*)this)->template get_label<SuperLabel>();
         }
 
         template<uint32_t SuperLabel>
         auto get_label() const
-            -> decltype(((label*)nullptr)->template _get_label<SuperLabel>(
+            -> decltype(((label*)0)->template _get_label<SuperLabel>(
                 select<Label == SuperLabel>()))
         {
-            return this->template _get_label<SuperLabel>(select<Label == SuperLabel>());
+            return this->template _get_label<SuperLabel>(
+                select<Label == SuperLabel>());
         }
     };
 }
