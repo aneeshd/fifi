78d133e754491e2f8a66a8b90cac29eb42845bd5
Peter Vingelmann <petya@master.com>
2014-02-24 02:35:14 +0100

    Only disable cpp files for MSVC

---
+++ src/fifi/binary.cpp
@@ -0,0 +1,19 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+// This file causes double definition warnings with MSVC
+#if !defined(_MSC_VER)
+
+#include "binary.hpp"
+
+namespace fifi
+{
+    const binary::value_type binary::max_value;
+    const binary::value_type binary::min_value;
+    const binary::order_type binary::order;
+    const bool binary::is_exact;
+}
+
+#endif
---
+++ src/fifi/binary8.cpp
@@ -0,0 +1,21 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+// This file causes double definition warnings with MSVC
+#if !defined(_MSC_VER)
+
+#include "binary8.hpp"
+
+namespace fifi
+{
+    const binary8::value_type binary8::max_value;
+    const binary8::value_type binary8::min_value;
+    const binary8::order_type binary8::order;
+    const binary8::degree_type binary8::degree;
+    const binary8::value_type binary8::prime;
+    const bool binary8::is_exact;
+}
+
+#endif
