d0c9fdb11b8cf3a9ef50603c3791cbff603348b7
Peter Vingelmann <petya@master.com>
2014-03-03 20:40:20 +0100

    Test has_basic_super type trait in region_dispatcher

--- src/fifi/region_dispatcher.hpp
+++ src/fifi/region_dispatcher.hpp
@@ -5,10 +5,26 @@
 
 #pragma once
 
+#include <type_traits>
+
 #include "region_dispatcher_specialization.hpp"
 
 namespace fifi
 {
+    template<typename T>
+    class has_basic_super
+    {
+        typedef uint8_t yes;
+        typedef uint32_t no;
+
+        template <typename U> static yes check(typename U::BasicSuper*);
+        template <typename U> static no  check(...);
+
+    public:
+
+        enum { value = (sizeof(check<T>(0)) == sizeof(yes)) };
+    };
+
     /// Helper class for easing the use of region_dispatcher_specialization.
     template<class Stack, class Super>
     class region_dispatcher :
@@ -16,9 +32,17 @@ namespace fifi
             typename Super::field_type, Stack,
             typename Stack::field_type, Super>
     {
+        template<bool B, class T>
+        struct reuse_basic_super_if { typedef typename T::BasicSuper type; };
+
+        template<class T>
+        struct reuse_basic_super_if<false, T> { typedef T type; };
+
     public:
 
-        typedef Super BasicSuper;
+        typedef typename reuse_basic_super_if<
+            has_basic_super<Super>::value, Super>::type BasicSuper;
+
         typedef region_dispatcher<Stack, Super> OptimizedSuper;
     };
 }
