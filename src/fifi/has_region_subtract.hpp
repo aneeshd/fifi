a86105ecdd29bdd89bc68700153f0268373ed0b1
Morten V. Pedersen <morten@mortenvp.dk>
2014-03-03 22:56:31 +0100

    Adding unit tests for the compiler function checks

---
+++ src/fifi/has_region_subtract.hpp
@@ -0,0 +1,44 @@
+// Copyright Steinwurf ApS 2014
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <utility>
+
+
+namespace fifi
+{
+    /// Type trait helper allows compile time detection of whether an
+    /// encoder contains a layer with the member function
+    /// region_subtract(value_type*,value_type*,uint32_t)
+    ///
+    /// Example:
+    ///
+    /// typedef fifi::simple_online online;
+    ///
+    /// if(kodo::has_region_subtract<online>::value)
+    /// {
+    ///     // Do something here
+    /// }
+    ///
+    template<typename T>
+    struct has_region_subtract
+    {
+    private:
+
+        template<typename U>
+        static auto test(int) ->
+            decltype(std::declval<U>().region_subtract(0,0,0), uint32_t());
+
+        template<typename> static uint8_t test(...);
+
+    public:
+
+        static const bool value = sizeof(decltype(test<T>(0))) == 4;
+    };
+}
+
---
+++ test/src/test_has_region_multiply_subtract.cpp
@@ -0,0 +1,38 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#include <gtest/gtest.h>
+
+#include <fifi/has_region_multiply_subtract.hpp>
+#include <fifi/full_table.hpp>
+#include <fifi/simple_online.hpp>
+
+namespace fifi
+{
+    // Put dummy layers and tests classes in an anonymous namespace
+    // to avoid violations of ODF (one-definition-rule) in other
+    // translation units
+    namespace
+    {
+        struct dummy_stack
+        { };
+    }
+}
+
+TEST(TestHasRegionMultiplySubtract, api)
+{
+    EXPECT_FALSE(fifi::has_region_multiply_subtract<fifi::dummy_stack>::value);
+
+    EXPECT_TRUE(fifi::has_region_multiply_subtract<
+                    fifi::simple_online<fifi::binary>>::value);
+
+    EXPECT_TRUE(fifi::has_region_multiply_subtract<
+                    fifi::full_table<fifi::binary8>>::value);
+
+    EXPECT_FALSE(fifi::has_region_multiply_subtract<uint32_t>::value);
+}
+
+
+
