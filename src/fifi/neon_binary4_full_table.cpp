50a900b5deda66c767ff7f01b0698c28c76dfba8
Peter Vingelmann <petya@master.com>
2014-03-24 20:08:12 +0100

    Add NEON intrinsics for Binary4

--- src/fifi/neon_binary4_full_table.cpp
+++ src/fifi/neon_binary4_full_table.cpp
@@ -15,40 +15,39 @@
 #include "simple_online_arithmetic.hpp"
 #include "final.hpp"
 
-#include "neon_binary8_full_table.hpp"
+#include "neon_binary4_full_table.hpp"
 
 namespace fifi
 {
 
 #ifdef PLATFORM_NEON
 
-    neon_binary8_full_table::neon_binary8_full_table()
+    neon_binary4_full_table::neon_binary4_full_table()
     {
-        simple_online_arithmetic<final<binary8>> field;
+        simple_online_arithmetic<final<binary4>> field;
 
-        m_table_one.resize(256 * 16);
-        m_table_two.resize(256 * 16);
+        m_table_one.resize(16 * 16);
+        m_table_two.resize(16 * 16);
 
         assert(((uintptr_t) &m_table_one[0] % 16) == 0);
         assert(((uintptr_t) &m_table_two[0] % 16) == 0);
 
         // Iterate over all elements in the field
-        for (uint32_t i = 0; i < 256; ++i)
+        for (uint32_t i = 0; i < 16; ++i)
         {
-            // Only take the constants whose first 4 bits are zero
             for (uint32_t j = 0; j < 16; ++j)
             {
-                // Calculate 8-bit product with the low-half
-                auto v1 = field.multiply(i, j);
-                m_table_one[i * 16 + j] = v1;
-                // Calculate 8-bit product with the high-half
-                auto v2 = field.multiply(i, j << 4);
-                m_table_two[i * 16 + j] = v2;
+                auto v = field.multiply(i, j);
+
+                // Store the 8-bit product in the low-half table
+                m_table_one[i * 16 + j] = v & 0x0f;
+                // Store the shifted 8-bit product in the high-half table
+                m_table_two[i * 16 + j] = (v << 4) & 0xf0;
             }
         }
     }
 
-    void neon_binary8_full_table::region_add(
+    void neon_binary4_full_table::region_add(
         value_type* dest, const value_type* src, uint32_t length) const
     {
         assert(dest != 0);
@@ -74,14 +73,14 @@ namespace fifi
         }
     }
 
-    void neon_binary8_full_table::region_subtract(
+    void neon_binary4_full_table::region_subtract(
         value_type* dest, const value_type* src, uint32_t length) const
     {
         // In a binary extension field addition is the same as subtraction
         region_add(dest, src, length);
     }
 
-    void neon_binary8_full_table::region_multiply_constant(
+    void neon_binary4_full_table::region_multiply_constant(
         value_type* dest, value_type constant, uint32_t length) const
     {
         assert(dest != 0);
@@ -91,6 +90,10 @@ namespace fifi
         // We loop 16 bytes at-a-time so we calculate how many loops we need
         uint32_t simd_size = length / granularity();
         assert(simd_size > 0);
+
+        // The constant is packed, so we need just either the high or
+        // low 4 bits to get constant value
+        constant = constant & 0xf;
 
         // Initialize the look-up tables
         // Load the 16-byte row that contains pre-calculated multiplication
@@ -117,7 +120,7 @@ namespace fifi
             // Perform 8 simultaneous table lookups to multiply the low-half
             // The lookup is performed twice due to NEON restrictions
             l = vcombine_u8(vtbl2_u8(table1, vget_low_u8(l)),
-                            vtbl2_u8(table1, vget_high_u8(l)));
+                vtbl2_u8(table1, vget_high_u8(l)));
             // Apply mask2 to get the high-half of the data
             uint8x16_t h = vandq_u8(q0, mask2);
             // Right shift the high-half by 4 bits to get values in [0,15]
@@ -125,7 +128,7 @@ namespace fifi
             // Perform table lookup with these indices to multiply the high-half
             // The lookup is performed twice due to NEON restrictions
             h = vcombine_u8(vtbl2_u8(table2, vget_low_u8(h)),
-                            vtbl2_u8(table2, vget_high_u8(h)));
+                vtbl2_u8(table2, vget_high_u8(h)));
             // Xor the high and low halves together to get the final result
             uint8x16_t result = veorq_u8(h, l);
             // Store the result in the destination buffer
@@ -133,7 +136,7 @@ namespace fifi
         }
     }
 
-    void neon_binary8_full_table::region_multiply_add(value_type* dest,
+    void neon_binary4_full_table::region_multiply_add(value_type* dest,
         const value_type* src, value_type constant, uint32_t length) const
     {
         assert(dest != 0);
@@ -144,6 +147,10 @@ namespace fifi
         // We loop 16 bytes at-a-time so we calculate how many loops we need
         uint32_t simd_size = length / granularity();
         assert(simd_size > 0);
+
+        // The constant is packed, so we need just either the high or
+        // low 4 bits to get constant value
+        constant = constant & 0xf;
 
         // Initialize the look-up tables
         // Load the 16-byte row that contains pre-calculated multiplication
@@ -194,39 +201,39 @@ namespace fifi
         }
     }
 
-    void neon_binary8_full_table::region_multiply_subtract(value_type* dest,
+    void neon_binary4_full_table::region_multiply_subtract(value_type* dest,
         const value_type* src, value_type constant, uint32_t length) const
     {
         // In a binary extension field addition is the same as subtraction
         region_multiply_add(dest, src, constant, length);
     }
 
-    uint32_t neon_binary8_full_table::alignment() const
+    uint32_t neon_binary4_full_table::alignment() const
     {
         return 1U;
     }
 
-    uint32_t neon_binary8_full_table::max_alignment() const
+    uint32_t neon_binary4_full_table::max_alignment() const
     {
         return alignment();
     }
 
-    uint32_t neon_binary8_full_table::granularity() const
+    uint32_t neon_binary4_full_table::granularity() const
     {
         // We are working over 16 bytes (128 bits) at a time, so we
-        // require a length granularity of 16. We expect that binary8
+        // require a length granularity of 16. We expect that binary4
         // uses uint8_t as value_type
         static_assert(std::is_same<value_type, uint8_t>::value,
-            "Here we expect binary8 to use uint8_t as value_type");
+            "Here we expect binary4 to use uint8_t as value_type");
         return 16U;
     }
 
-    uint32_t neon_binary8_full_table::max_granularity() const
+    uint32_t neon_binary4_full_table::max_granularity() const
     {
         return granularity();
     }
 
-    bool neon_binary8_full_table::enabled() const
+    bool neon_binary4_full_table::enabled() const
     {
         static cpuid::cpuinfo info;
         return info.has_neon();
@@ -234,73 +241,73 @@ namespace fifi
 
 #else
 
-    neon_binary8_full_table::neon_binary8_full_table()
+    neon_binary4_full_table::neon_binary4_full_table()
     { }
 
-    void neon_binary8_full_table::region_add(
+    void neon_binary4_full_table::region_add(
         value_type*, const value_type*, uint32_t) const
     {
         // Not implemented
         assert(0);
     }
 
-    void neon_binary8_full_table::region_subtract(
+    void neon_binary4_full_table::region_subtract(
         value_type*, const value_type*, uint32_t) const
     {
         // Not implemented
         assert(0);
     }
 
-    void neon_binary8_full_table::region_multiply_constant(
+    void neon_binary4_full_table::region_multiply_constant(
         value_type*, value_type, uint32_t) const
     {
         // Not implemented
         assert(0);
     }
 
-    void neon_binary8_full_table::region_multiply_add(
+    void neon_binary4_full_table::region_multiply_add(
         value_type*, const value_type*, value_type, uint32_t) const
     {
         // Not implemented
         assert(0);
     }
 
-    void neon_binary8_full_table::region_multiply_subtract(
+    void neon_binary4_full_table::region_multiply_subtract(
         value_type*, const value_type*, value_type, uint32_t) const
     {
         // Not implemented
         assert(0);
     }
 
-    uint32_t neon_binary8_full_table::alignment() const
+    uint32_t neon_binary4_full_table::alignment() const
     {
         // Not implemented
         assert(0);
         return 0;
     }
 
-    uint32_t neon_binary8_full_table::max_alignment() const
+    uint32_t neon_binary4_full_table::max_alignment() const
     {
         // Not implemented
         assert(0);
         return 0;
     }
 
-    uint32_t neon_binary8_full_table::granularity() const
+    uint32_t neon_binary4_full_table::granularity() const
     {
         // Not implemented
         assert(0);
         return 0;
     }
 
-    uint32_t neon_binary8_full_table::max_granularity() const
+    uint32_t neon_binary4_full_table::max_granularity() const
     {
         // Not implemented
         assert(0);
         return 0;
     }
 
-    bool neon_binary8_full_table::enabled() const
+    bool neon_binary4_full_table::enabled() const
     {
         return false;
     }
---
+++ test/src/test_neon_binary4_full_table.cpp
@@ -0,0 +1,63 @@
+// Copyright Steinwurf ApS 2011-2014.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#include <gtest/gtest.h>
+
+#include <cpuid/cpuinfo.hpp>
+
+#include <fifi/neon_binary4_full_table.hpp>
+
+#include "fifi_unit_test/helper_test_arithmetic.hpp"
+#include "fifi_unit_test/helper_test_packed_arithmetic.hpp"
+#include "fifi_unit_test/helper_test_region_arithmetic.hpp"
+
+
+TEST(test_neon_binary4_full_table, region_add)
+{
+    fifi::neon_binary4_full_table stack;
+    if (stack.enabled())
+    {
+        check_results_region_add<fifi::neon_binary4_full_table>();
+    }
+}
+
+TEST(test_neon_binary4_full_table, region_subtract)
+{
+    fifi::neon_binary4_full_table stack;
+    if (stack.enabled())
+    {
+        check_results_region_subtract<fifi::neon_binary4_full_table>();
+    }
+}
+
+TEST(test_neon_binary4_full_table, region_multiply_constant)
+{
+    fifi::neon_binary4_full_table stack;
+    if (stack.enabled())
+    {
+        check_results_region_multiply_constant<
+            fifi::neon_binary4_full_table>();
+    }
+}
+
+TEST(test_neon_binary4_full_table, region_multiply_add)
+{
+    fifi::neon_binary4_full_table stack;
+    if (stack.enabled())
+    {
+        check_results_region_multiply_add<
+            fifi::neon_binary4_full_table>();
+    }
+}
+
+TEST(test_neon_binary4_full_table, region_multiply_subtract)
+{
+    fifi::neon_binary4_full_table stack;
+    if (stack.enabled())
+    {
+        check_results_region_multiply_subtract<
+            fifi::neon_binary4_full_table>();
+    }
+}
