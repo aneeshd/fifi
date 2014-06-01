dd2557668c50fad0601a1fe5db0ee00e5cbd2b3a
Morten V. Pedersen <morten@mortenvp.com>
2014-06-01 15:06:51 +0200

    Fix-up failing unit test

--- test/src/test_basic_region_dispatcher.cpp
+++ test/src/test_basic_region_dispatcher.cpp
@@ -10,8 +10,6 @@
 
 #include <stub/call.hpp>
 
-#include "fifi_unit_test/helper_region_info.hpp"
-
 // Put dummy layers and tests classes in an anonymous namespace
 // to avoid violations of ODF (one-definition-rule) in other
 // translation units
@@ -56,18 +54,14 @@ namespace
             m_multiply_constant(dest,constant,length);
         }
 
-        void region_multiply_add(value_type* dest,
-                                 const value_type* src,
-                                 value_type constant,
-                                 uint32_t length) const
+        void region_multiply_add(value_type* dest, const value_type* src,
+                                 value_type constant, uint32_t length) const
         {
             m_multiply_add(dest,src,constant,length);
         }
 
-        void region_multiply_subtract(value_type* dest,
-                                      const value_type* src,
-                                      value_type constant,
-                                      uint32_t length) const
+        void region_multiply_subtract(value_type* dest, const value_type* src,
+                                      value_type constant,uint32_t length) const
         {
             m_multiply_subtract(dest,src,constant,length);
         }
@@ -94,7 +88,7 @@ namespace
             m_multiply_subtract;
     };
 
-    class dummy_stack : public kodo::basic_region_dispatcher<dummy_super>
+    class dummy_stack : public fifi::basic_region_dispatcher<dummy_super>
     { };
 }
 
@@ -112,5 +106,24 @@ TEST(basic_region_dispatcher, api)
     auto multiply_add = stack.dispatch_region_multiply_add();
     auto multiply_subtract = stack.dispatch_region_multiply_subtract();
 
+    add(0,0,0);
+    EXPECT_TRUE(stack.m_add.called_once_with(0,0,0));
 
+    subtract(0,0,1);
+    EXPECT_TRUE(stack.m_subtract.called_once_with(0,0,1));
+
+    divide(0,0,2);
+    EXPECT_TRUE(stack.m_divide.called_once_with(0,0,2));
+
+    multiply(0,0,3);
+    EXPECT_TRUE(stack.m_multiply.called_once_with(0,0,3));
+
+    multiply_constant(0,1,3);
+    EXPECT_TRUE(stack.m_multiply_constant.called_once_with(0,1,3));
+
+    multiply_add(0,0,1,3);
+    EXPECT_TRUE(stack.m_multiply_add.called_once_with(0,0,1,3));
+
+    multiply_subtract(0,0,2,3);
+    EXPECT_TRUE(stack.m_multiply_subtract.called_once_with(0,0,2,3));
 }
